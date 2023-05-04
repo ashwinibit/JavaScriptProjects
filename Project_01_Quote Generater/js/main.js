81df51c6e526628edf08130d4dd87f87f378e8df
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-05-04 09:38:34 +0530

    button working

--- Project_01_Quote Generater/js/main.js
+++ Project_01_Quote Generater/js/main.js
@@ -1,8 +1,26 @@
 fetch("https://type.fit/api/quotes").then(function(response) {
     return response.json();
 }).then(function(data) {
-    console.log(data);
+    console.log();
+    // console.log(data.length)
+    document.getElementById("quote").innerText += Object.values(data[0])[0]
+    document.getElementById("qouteBy").innerHTML += Object.values(data[0])[1]
+    document.getElementById('loader').classList.add('hide-loader')
+    document.getElementById('content').style.visibility = 'visible'
+
+    document.getElementById('newQuote').addEventListener('click', function(){
+        let i = Math.floor((Math.random() * data.length))
+        document.getElementById("quote").innerText = Object.values(data[i])[0]
+        document.getElementById("qouteBy").innerHTML = Object.values(data[i])[1]
+    
+    })
 
 })
+
+
+
+
+
+
 
 
