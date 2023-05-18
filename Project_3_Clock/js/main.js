75992c4bf3d422111c8462776587b09fd4add3c4
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-05-18 22:59:48 +0530

    little CSS

--- Project_3_Clock/js/main.js
+++ Project_3_Clock/js/main.js
@@ -5,8 +5,22 @@ function digitalClock(){
     let m = today.getMinutes();
     let s = today.getSeconds();
     let d = today.getDay();
+    let session = 'AM';
+    
+    if(h == 0){
+        h =12;
+    }
+    if(h >12){
+        h = h - 12;
+        session = 'PM';
+    }
 
-    document.getElementById('digitalClock').innerHTML = h + ":" + m + ":" + s;
+    h = (h <10) ? '0' + h : h;
+    m = (m <10) ? '0' + m : m;
+    s = (s <10) ? '0' + s : s;
+
+
+    document.getElementById('digitalClock').innerHTML = h + ":" + m + ":" + s + " " + session;
     setTimeout(digitalClock, 1000)
 }
 
