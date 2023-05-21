05e0256fff26f098580255eeb950bb5f7162421d
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-05-21 18:06:48 +0530

    small changes

--- Project_4_Bad Jokes/js/main.js
+++ Project_4_Bad Jokes/js/main.js
@@ -2,6 +2,7 @@
 function newJoke(){
     document.getElementById('jokePart').innerHTML = "";
     document.querySelector('.loader').classList.remove("hide-loader");
+    document.querySelector('.container').classList.add("addBlur");
     let apiUrl = fetch('https://icanhazdadjoke.com/',{
         // request header
         headers:{
@@ -13,6 +14,7 @@ function newJoke(){
     }).then((val2) => {
         console.log(val2.joke);
         document.querySelector('.loader').classList.add("hide-loader");
+        document.querySelector('.container').classList.remove("addBlur");
         document.getElementById('jokePart').innerHTML = val2.joke;
     })
 }
