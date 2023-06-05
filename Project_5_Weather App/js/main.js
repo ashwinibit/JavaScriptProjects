4d3892f976c2d4e417f0a201bf6b012125e032e4
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-06-05 23:55:01 +0530

    Code

---
+++ Project_5_Weather App/js/main.js
@@ -0,0 +1,9 @@
+
+
+const search = document.getElementById('submitButton');
+
+search.addEventListener('click', () => {
+    const ApiKey = "9739a21be4db4fee0d1da331a511bc5b"
+    const city = document.getElementById("searchBox").value;
+    
+});
