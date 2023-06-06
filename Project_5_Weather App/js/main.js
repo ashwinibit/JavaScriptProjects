6cf8d5fb9ebd884d77a34d40ed4684d29fc5c263
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-06-06 23:12:35 +0530

    Code

--- Project_5_Weather App/js/main.js
+++ Project_5_Weather App/js/main.js
@@ -5,5 +5,5 @@ const search = document.getElementById('submitButton');
 search.addEventListener('click', () => {
     const ApiKey = "9739a21be4db4fee0d1da331a511bc5b"
     const city = document.getElementById("searchBox").value;
-    
+    fetch('https://api.openweathermap.org/data/3.0/weather?q=${city}&units=metric&appid=${ApiKey')
 });
